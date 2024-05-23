48f1241ebc4101f69d55f007579c9dd95b5c7267
Steven Ontong <steven@journeyapps.com>
2024-05-23 12:23:51 +0200

    add export for sqlite3_web

---
+++ lib/sqlite3_web.dart
@@ -0,0 +1,5 @@
+/// Re-exports [sqlite3_web](https://pub.dev/packages/sqlite3_web) to expose sqlite3_web without
+/// adding it as a direct dependency.
+library;
+
+export 'package:sqlite3_web/sqlite3_web.dart';
