4c4f6ccda57ce57aaae3b77e07922228bb6673e8
Simon Binder <simon@journeyapps.com>
2025-05-19 16:00:26 +0200

    Use Homebrew SQLite if available

--- packages/sqlite_async/test/utils/native_test_utils.dart
+++ packages/sqlite_async/test/utils/native_test_utils.dart
@@ -26,6 +26,16 @@ class TestSqliteOpenFactory extends TestDefaultSqliteOpenFactory {
     sqlite_open.open.overrideFor(sqlite_open.OperatingSystem.linux, () {
       return DynamicLibrary.open(sqlitePath);
     });
+
+    sqlite_open.open.overrideFor(sqlite_open.OperatingSystem.macOS, () {
+      // Prefer using Homebrew's SQLite which allows loading extensions.
+      const fromHomebrew = '/opt/homebrew/opt/sqlite/lib/libsqlite3.dylib';
+      if (File(fromHomebrew).existsSync()) {
+        return DynamicLibrary.open(fromHomebrew);
+      }
+
+      return DynamicLibrary.open('libsqlite3.dylib');
+    });
   }
 
   @override
