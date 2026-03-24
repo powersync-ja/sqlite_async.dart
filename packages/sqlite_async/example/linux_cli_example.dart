e6f234700fb07a413655e347011babf1657ee28d
Simon Binder <simon@journeyapps.com>
2026-03-24 14:15:05 +0100

    Remove linux_cli_example.dart

--- packages/sqlite_async/example/linux_cli_example.dart
+++
@@ -1,8 +0,0 @@
-import 'package:sqlite_async/sqlite_async.dart';
-
-void main() async {
-  final db = SqliteDatabase(path: 'test.db');
-  final version = await db.get('SELECT sqlite_version() as version');
-  print("Version: ${version['version']}");
-  await db.close();
-}
