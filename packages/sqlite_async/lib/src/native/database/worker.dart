8f2a1011469c0732dba220dc08c9045e627ef01e
Simon Binder <simon@journeyapps.com>
2026-03-25 17:21:29 +0100

    Document motivation for isolate worker

--- packages/sqlite_async/lib/src/native/database/worker.dart
+++ packages/sqlite_async/lib/src/native/database/worker.dart
@@ -3,28 +3,52 @@ import 'dart:isolate';
 
 import 'package:async/async.dart';
 
-final class WorkItem {
-  final SendPort completePort;
-  final Object? Function() task;
-
-  WorkItem(this.completePort, this.task);
-}
-
+/// A long-lived isolate running work items sent as closures.
+///
+/// Ideally, we should be able to use [Isolate.run] for this purpose. In
+/// benchmarks however, this seems to be substantially faster than spawning new
+/// isolates.
 final class IsolateWorker {
-  final Isolate isolate;
-  final SendPort sendCommands;
+  final Isolate _isolate;
 
-  IsolateWorker._(this.isolate, this.sendCommands);
+  final ReceivePort _receiveResponses = ReceivePort('isolate worker');
+  final SendPort _sendCommands;
 
-  Future<T> run<T>(FutureOr<T> Function() task) async {
-    final port = ReceivePort();
-    sendCommands.send(WorkItem(port.sendPort, task));
+  final Map<int, Completer<Object?>> _outstandingWorkItems = {};
+  int _nextWorkItem = 0;
 
-    return (await Result.release(port.first.then((r) => r as Result<Object?>)))
-        as T;
+  IsolateWorker._(this._isolate, this._sendCommands) {
+    _receiveResponses.listen((Object? message) {
+      final WorkResult(:id, :result) = message as WorkResult;
+      if (_outstandingWorkItems.remove(id) case final completer?) {
+        switch (result) {
+          case ValueResult(:final value):
+            completer.complete(value);
+          case ErrorResult(:final error, :final stackTrace):
+            completer.completeError(error, stackTrace);
+        }
+      }
+    });
   }
 
-  void close() => isolate.kill();
+  Future<T> run<T>(FutureOr<T> Function() task) async {
+    final id = _nextWorkItem++;
+    final completer = _outstandingWorkItems[id] = Completer();
+
+    _sendCommands.send(WorkItem(id, _receiveResponses.sendPort, task));
+    return (await completer.future) as T;
+  }
+
+  void close() {
+    _isolate.kill();
+    for (final pending in _outstandingWorkItems.values) {
+      // This really shouldn't happen, but it's better than not having a future
+      // that doesn't complete.
+      pending.completeError(StateError('Worker closed'));
+    }
+    _outstandingWorkItems.clear();
+    _receiveResponses.close();
+  }
 
   static Future<IsolateWorker> spawn() async {
     final receiveSendPort = ReceivePort();
@@ -38,16 +62,37 @@ final class IsolateWorker {
     final receiveTasks = ReceivePort('receive tasks');
     sendPort.send(receiveTasks.sendPort);
 
-    await for (final WorkItem(:completePort, :task)
-        in receiveTasks.cast<WorkItem>()) {
-      final result = await runZonedGuarded(
-        () => Result.capture(Future.sync(task)),
-        (Object e, StackTrace s) {
-          completePort.send(ErrorResult(e, s));
-        },
-      );
-
-      completePort.send(result);
+    await for (final item in receiveTasks.cast<WorkItem>()) {
+      await item.handle();
     }
   }
+}
+
+final class WorkItem {
+  final int id;
+  final SendPort completePort;
+  final Object? Function() task;
+
+  WorkItem(this.id, this.completePort, this.task);
+
+  Future<void> handle() async {
+    final result = await runZonedGuarded(
+      () => Result.capture(Future.sync(task)),
+      (Object e, StackTrace s) {
+        completePort.send(WorkResult(id, ErrorResult(e, s)));
+      },
+    );
+
+    if (result != null) {
+      // For errors, we would have already sent the response.
+      completePort.send(WorkResult(id, result));
+    }
+  }
+}
+
+final class WorkResult {
+  final int id;
+  final Result<Object?> result;
+
+  WorkResult(this.id, this.result);
 }
